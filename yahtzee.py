93bb9558fda797a5cef5abdc4cb2e7abb58303d4
Christine <christiinele@gmail.com>
2020-12-11 10:43:15 -0800

    Create score card function

--- yahtzee.py
+++ yahtzee.py
@@ -13,7 +13,11 @@ def create_scorecard() -> dict:
     :return: dictionary with players' scores
     """
 
-    return None
+    score_card = {"Ones": -1, "Twos": -1, "Threes": -1, "Fours": -1, "Fives": -1, "Sixes": -1,
+                  "Three Of A Kind": -1, "Four Of A Kind": -1, "Full House": -1,
+                  "Small Straight": -1, "Large Straight": -1, "Yahtzee": -1, "Chance": -1}
+
+    return score_card
 
 
 def print_score(scorecard: dict):
